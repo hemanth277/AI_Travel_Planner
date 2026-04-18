fe013fb5b616d33fa0cdfe49e36ca88f9d69c92a
hemanth277 <hemanthasapu0877@gmail.com>
2026-04-18 10:45:23 +0530

    Build: Swapped logic to use dist folder and removed build outDir override

--- backend/server.js
+++ backend/server.js
@@ -333,10 +333,10 @@ app.post('/api/plan-trip', async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.listen(PORT, () => {
